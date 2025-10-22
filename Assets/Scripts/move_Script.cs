b2c5f650f616445fdb37df2c674c65067f6f1a5b
Marson-mereketenge <marlonjimenezpardo@alumnos.ilernasevilla.com>
2025-10-22 11:49:06 +0200

    IntentarQueAlgoSeMueva

---
+++ Assets/Scripts/move_Script.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem
+
+public class move_Script : MonoBehaviour
+{
+    float moveForce;
+    float jumpForce;
+
+    Vector3 input;
+
+    Rigidbody rb;
+    PlayerInput playerInput;
+    void Start()
+    {
+        moveForce 15f;
+        jumpForce 10f;
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+    public void Jump(InputAction.CallbackContext callbackContex)
+    { 
+        if (callbackContex.performed) 
+        {
+            rb.AddForce(Vector3.up * jumpForce)
+            Debug.Log("Saltando");
+            Debug.Log(callbackContex.phase);
+        }
+    }
+    void Update()
+    {
+        input = playerInput.actions["Move"].ReadValue<Vector2>();   
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(input.x, 0f, input.y) * moveForce);
+    }
+}
