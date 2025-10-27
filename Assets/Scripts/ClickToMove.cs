7e6bc73428620fc2b44ff080f34cc1898fed9f59
Marson-mereketenge <marlonjimenezpardo@alumnos.ilernasevilla.com>
2025-10-27 10:30:41 +0100

    NavMesh

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -1,36 +1,38 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ClickToMove : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
     Rigidbody rb;
     Vector3 destination;
     [SerializeField] Transform destinoDumie;
+    NavMeshAgent agent;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        moveSpeed = 3f;
+        agent = GetComponent<NavMeshAgent>();
+        
     }
     void Update()
     {
-        destination = destinoDumie.position;
+    
+        
         if (Input.GetMouseButtonDown(1))
         {
+ 
             HandleCkick();
         }
     }
 
     private void HandleCkick()
     {
-        StartCoroutine(MoveToPosition(destination));
-    }
-
-    IEnumerator MoveToPosition(Vector3 _destination)
-    {
-        Vector3 moveDirection = _destination - transform.position;
-        moveDirection = moveDirection.normalized;
-        rb.AddForce(moveDirection * moveSpeed, ForceMode.VelocityChange);
-        yield return null;    
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100f))
+        {
+            destinoDumie.position = hit.point;
+            agent.destination = destinoDumie.position;
+        }
+        
     }
 }
