4bd9643286204a8ce97951de48b31528de1b3a81
Marson-mereketenge <marlonjimenezpardo@alumnos.ilernasevilla.com>
2025-11-28 21:28:41 +0100

    Entrega_3_Final.1
    1. Todos los personajes tienen animaciones
    2. Los personajes reproducen idle
    3. Los botones del chomper funcionan
    //Porque coño no se reproducen las animaciones además del idle

---
+++ Assets/Scripts/Arreglar_Chomper_Animator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Arreglar_Chomper_Animator : MonoBehaviour
+{
+     // Este método será llamado por el AnimationEvent
+    public void PlayStep()
+    {
+    }
+    public void Grunt()
+    {
+    }
+}
